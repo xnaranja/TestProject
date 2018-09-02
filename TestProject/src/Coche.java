1a9a5de96c68c2c6fb287ed88e21a4dc0c2b4127
adminprofe <adminprofe@lyx1>
2018-09-02 20:17:52 +0200

    Cuarto

---
+++ TestProject/src/Coche.java
@@ -0,0 +1,12 @@
+
+public class Coche {
+	
+	//atributo de clase
+	  private static int kilometrajeTotal = 0;
+	  
+	  // método de clase
+	  public static int getKilometrajeTotal() {
+	    return Coche.kilometrajeTotal;
+	  }
+
+}
